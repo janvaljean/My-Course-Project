b340840aae8ce30b69b1adc73a464cdc4ed3f96e
janvaljean <kurtuldu@gmx.de>
2023-04-07 00:44:33 +0200

    my courses change with icons

--- my-courses/src/Courses.jsx
+++ my-courses/src/Courses.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import Course from "./Course";
+import { useState } from "react";
+import { FaChevronCircleRight, FaChevronCircleLeft } from "react-icons/fa";
 
-const Courses = ({ courses, removeOneCourse, refreshCourses }) => {
-  console.log(courses.length);
+FaChevronCircleRight;
+FaChevronCircleLeft;
+
+const Courses = ({ courses, refreshCourses }) => {
+  const [index, setIndex] = useState(0);
+  const { content, title, price } = courses[index];
+
+  const prevCourse = () => {
+    if(index>0){
+      setIndex(index-1);
+      
+    }else{
+      setIndex(courses.length-1)
+     
+    }
+  };
+  const nextCourse = () => {
+    if(index<courses.length-1){
+      setIndex(index+1);
+      console.log(index);
+    }else{
+      setIndex(0)
+       console.log(index);
+    }
+  };
   return (
     <>
       <div className="courseMainDiv">
@@ -23,14 +48,27 @@ const Courses = ({ courses, removeOneCourse, refreshCourses }) => {
           </div>
         ) : (
           <div className="cardDiv">
-            {courses.map((course) => (
+            {/* {courses.map((course) => (
               <Course
                 key={course.id}
                 {...course}
                 removeOneCourse={removeOneCourse}
                 //  course={course}
               />
-            ))}
+            ))} */}
+            <button onClick={prevCourse} className="prevNextButton">
+              <FaChevronCircleLeft />
+            </button>
+            <div className="card">
+              <div className="cardTitlePrice">
+                <h2 className="cardTitle">{title}</h2>
+                <h4 className="cardPrice">{price}</h4>
+              </div>
+              <p>{content}</p>
+            </div>
+            <button onClick={nextCourse} className="prevNextButton">
+              <FaChevronCircleRight />
+            </button>
           </div>
         )}
       </div>
